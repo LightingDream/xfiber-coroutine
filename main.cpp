b8bd24b824fceca0a3bd00981e6d324e117ebdb8
LightingDream <2456700829@qq.com>
2022-09-07 12:22:27 +0800

    edit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,45 @@
 #include "xfiber.h"
+#include "xsocket.h"
+
+#include <string.h>
+#include <strings.h>
 #include <iostream>
+
 int main(int, char**) {
     XFiber xfiber;
+    // xfiber.CreateFiber([&]{
+    //     std::cout << "I am coroutine A" << std::endl;
+    //     std::cout << "I am coroutine A" << std::endl;
+    //     xfiber.Yield();
+    //     std::cout << "I am coroutine A" << std::endl;
+    //     std::cout << "I am coroutine A" << std::endl;
+    // });
+    // xfiber.CreateFiber([&]{
+    //     std::cout << "I am coroutine B" << std::endl;
+    //     std::cout << "I am coroutine B" << std::endl;
+    //     xfiber.Yield();
+    //     std::cout << "I am coroutine B" << std::endl;
+    //     std::cout << "I am coroutine B" << std::endl;
+    // });
     xfiber.CreateFiber([&]{
-        std::cout << "I am coroutine A" << std::endl;
-        std::cout << "I am coroutine A" << std::endl;
-        xfiber.Yield();
-        std::cout << "I am coroutine A" << std::endl;
-        std::cout << "I am coroutine A" << std::endl;
-    });
-    xfiber.CreateFiber([&]{
-        std::cout << "I am coroutine B" << std::endl;
-        std::cout << "I am coroutine B" << std::endl;
-        xfiber.Yield();
-        std::cout << "I am coroutine B" << std::endl;
-        std::cout << "I am coroutine B" << std::endl;
+        Listener listener;
+        listener.Listen(8888);
+        while(true) {
+            // sleep(2);
+            int connfd = listener.Accept();
+            if(connfd <= 0) continue;
+            xfiber.CreateFiber([&]{
+                char buf[1024] = {0};
+                while(true) {
+                    sleep(2);
+                    int len = ::read(connfd, buf, 1024);
+                    std::cout << "read : " << buf << std::endl;
+                    ::write(connfd, buf, strlen(buf));
+                    ::bzero(buf, sizeof buf);
+                }
+            });
+            xfiber.Yield();
+        }
     });
     xfiber.Dispatch();
     return 0;
